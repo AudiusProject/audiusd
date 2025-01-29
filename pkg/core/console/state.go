767f0bb703be4f6d4da7679bf2a8f1be9879eddb
Alec Savvy <alecsavvy@outlook.com>
2025-01-29 15:09:50 -0700

    fix mainnet-alpha counts

--- pkg/core/console/state.go
+++ pkg/core/console/state.go
@@ -49,6 +49,14 @@ func (state *State) recalculateState() {
 	ctx := context.Background()
 	logger := state.logger
 
+	status, err := state.rpc.Status(ctx)
+	if err != nil {
+		logger.Errorf("could not get node status: %v", err)
+	} else {
+		state.totalBlocks = status.SyncInfo.LatestBlockHeight
+		state.isSyncing = status.SyncInfo.CatchingUp
+	}
+
 	recentTransactions, err := state.db.GetRecentTxs(ctx)
 	if err != nil {
 		logger.Errorf("could not get recent txs: %v", err)
@@ -56,19 +64,12 @@ func (state *State) recalculateState() {
 		state.latestTransactions = recentTransactions
 	}
 
-	// on initial load
-	totalTxs, err := state.db.TotalTransactions(ctx)
+	// TODO: remove after audius-mainnet-alpha
+	totalTxs, err := state.db.GetTotalTxStats(ctx)
 	if err != nil {
 		logger.Errorf("could not get total txs: %v", err)
 	} else {
 		state.totalTransactions = totalTxs
-	}
-
-	totalBlocks, err := state.db.TotalBlocks(ctx)
-	if err != nil {
-		logger.Errorf("could not get total blocks: %v", err)
-	} else {
-		state.totalBlocks = totalBlocks
 	}
 
 	totalPlays, err := state.db.TotalTransactionsByType(ctx, server.TrackPlaysProtoName)
@@ -118,11 +119,6 @@ func (state *State) recalculateState() {
 	}
 
 	state.latestBlocks = latestBlocks
-
-	status, err := state.rpc.Status(ctx)
-	if err == nil {
-		state.isSyncing = status.SyncInfo.CatchingUp
-	}
 }
 
 func (state *State) Start() error {
