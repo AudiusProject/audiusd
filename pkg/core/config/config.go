e74cfbe71c5b38770570d84c069c0eacca5bf1c6
Alec Savvy <alecsavvy@outlook.com>
2025-02-05 16:36:53 -0700

    rm state log

--- pkg/core/config/config.go
+++ pkg/core/config/config.go
@@ -129,7 +129,7 @@ func ReadConfig(logger *common.Logger) (*Config, error) {
 
 	var cfg Config
 	// comet config
-	cfg.LogLevel = GetEnvWithDefault("audius_core_log_level", "p2p:none,mempool:none,rpc:none,state:info,*:error")
+	cfg.LogLevel = GetEnvWithDefault("audius_core_log_level", "p2p:none,mempool:none,rpc:none,*:error")
 	cfg.RootDir = GetEnvWithDefault("audius_core_root_dir", homeDir+"/.audiusd")
 	cfg.RPCladdr = GetEnvWithDefault("rpcLaddr", "tcp://0.0.0.0:26657")
 	cfg.P2PLaddr = GetEnvWithDefault("p2pLaddr", "tcp://0.0.0.0:26656")
