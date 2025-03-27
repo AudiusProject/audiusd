cde8297b65522011ef1bd897c29934b84ba5031e
Alec Savvy <alecsavvy@outlook.com>
2025-03-27 13:38:12 -0600

    this is kind of working

--- pkg/core/console/views/sandbox/sandbox.go
+++ pkg/core/console/views/sandbox/sandbox.go
@@ -3,28 +3,13 @@ package sandbox
 import (
 	"embed"
 	"net/http"
-	"strings"
 )
 
-// Embed sandbox.html and defaultCode.js
-//
-//go:embed sandbox.html defaultCode.js
+//go:embed sandbox.html
 var SandboxAssets embed.FS
 
 // ServeSandbox serves the embedded sandbox files
 func ServeSandbox(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	if strings.HasSuffix(path, "defaultCode.js") {
-		// Serve the JavaScript file
-		data, err := SandboxAssets.ReadFile("defaultCode.js")
-		if err != nil {
-			http.Error(w, "failed to read defaultCode.js", http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/javascript")
-		w.Write(data)
-		return
-	}
 	// Serve the HTML file
 	data, err := SandboxAssets.ReadFile("sandbox.html")
 	if err != nil {
