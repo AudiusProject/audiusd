a08b95633445cba88e2b062b29f3cdf7b58c6bd3
Alec Savvy <alecsavvy@outlook.com>
2025-04-01 14:45:22 -0600

    add dev gate

--- pkg/core/console/upload.go
+++ pkg/core/console/upload.go
@@ -3,5 +3,8 @@ package console
 import "github.com/labstack/echo/v4"
 
 func (cs *Console) uploadPage(c echo.Context) error {
-	return cs.views.RenderUploadPageView(c)
+	if cs.config.Environment == "dev" {
+		return cs.views.RenderUploadPageView(c)
+	}
+	return c.String(200, "not implemented")
 }
