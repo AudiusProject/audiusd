99c21fabfb507d7249dce04908bc4ccd31f6eacf
Alec Savvy <alecsavvy@outlook.com>
2025-04-08 10:57:38 -0600

    add all reward types

--- pkg/core/rewards/rewards.go
+++ pkg/core/rewards/rewards.go
@@ -5,23 +5,323 @@ var (
 		{
 			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
 			Amount:       1,
+			RewardId:     "p",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1,
+			RewardId:     "e",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1,
+			RewardId:     "u",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1,
+			RewardId:     "r",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1,
+			RewardId:     "rv",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1,
+			RewardId:     "rd",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       5,
+			RewardId:     "v",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1,
+			RewardId:     "m",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1000,
+			RewardId:     "tt",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1000,
+			RewardId:     "tut",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       100,
+			RewardId:     "tp",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       2,
+			RewardId:     "ft",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       2,
+			RewardId:     "fp",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       5,
+			RewardId:     "b",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       5,
+			RewardId:     "s",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1,
+			RewardId:     "o",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1,
 			RewardId:     "c",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       25,
+			RewardId:     "p1",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       100,
+			RewardId:     "p2",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       1000,
+			RewardId:     "p3",
+		},
+		{
+			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			Amount:       100,
+			RewardId:     "t",
 		},
 	}
 
 	StageRewards = []Reward{
 		{
-			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1,
+			RewardId:     "p",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1,
+			RewardId:     "e",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1,
+			RewardId:     "u",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1,
+			RewardId:     "r",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1,
+			RewardId:     "rv",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1,
+			RewardId:     "rd",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       5,
+			RewardId:     "v",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1,
+			RewardId:     "m",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1000,
+			RewardId:     "tt",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1000,
+			RewardId:     "tut",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       100,
+			RewardId:     "tp",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       2,
+			RewardId:     "ft",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       2,
+			RewardId:     "fp",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       5,
+			RewardId:     "b",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       5,
+			RewardId:     "s",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1,
+			RewardId:     "o",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
 			Amount:       1,
 			RewardId:     "c",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       25,
+			RewardId:     "p1",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       100,
+			RewardId:     "p2",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       1000,
+			RewardId:     "p3",
+		},
+		{
+			ClaimWallets: []string{"0x8fcFA10Bd3808570987dbb5B1EF4AB74400FbfDA"},
+			Amount:       100,
+			RewardId:     "t",
 		},
 	}
 
 	ProdRewards = []Reward{
 		{
-			ClaimWallets: []string{"0x73EB6d82CFB20bA669e9c178b718d770C49BB52f"},
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1,
+			RewardId:     "p",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1,
+			RewardId:     "e",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1,
+			RewardId:     "u",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1,
+			RewardId:     "r",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1,
+			RewardId:     "rv",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1,
+			RewardId:     "rd",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       5,
+			RewardId:     "v",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1,
+			RewardId:     "m",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1000,
+			RewardId:     "tt",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1000,
+			RewardId:     "tut",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       100,
+			RewardId:     "tp",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       2,
+			RewardId:     "ft",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       2,
+			RewardId:     "fp",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       5,
+			RewardId:     "b",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       5,
+			RewardId:     "s",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1,
+			RewardId:     "o",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
 			Amount:       1,
 			RewardId:     "c",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       25,
+			RewardId:     "p1",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       100,
+			RewardId:     "p2",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       1000,
+			RewardId:     "p3",
+		},
+		{
+			ClaimWallets: []string{"0xf1a1Bd34b2Bc73629aa69E50E3249E89A3c16786"},
+			Amount:       100,
+			RewardId:     "t",
 		},
 	}
 )
