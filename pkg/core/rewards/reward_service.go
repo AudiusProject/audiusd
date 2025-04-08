ad2f6f1c449bc157197456ed43436d54dd528217
Alec Savvy <alecsavvy@outlook.com>
2025-04-08 11:21:00 -0600

    refactor a tad

--- pkg/core/rewards/reward_service.go
+++ pkg/core/rewards/reward_service.go
@@ -12,20 +12,48 @@ type RewardService struct {
 }
 
 func NewRewardService(config *config.Config) *RewardService {
-	rewards := []Reward{}
-	if config.Environment == "dev" {
-		rewards = append(rewards, DevRewards...)
-	} else if config.Environment == "stage" {
-		rewards = append(rewards, StageRewards...)
-	} else if config.Environment == "prod" {
-		rewards = append(rewards, ProdRewards...)
+	// Create a deep copy of BaseRewards
+	rewards := make([]Reward, len(BaseRewards))
+	copy(rewards, BaseRewards)
+
+	// Get the appropriate pubkeys and reward extensions based on environment
+	var pubkeys []string
+	var extensions []Reward
+	switch config.Environment {
+	case "dev":
+		pubkeys = DevPubkeys
+		extensions = DevRewardExtensions
+	case "stage":
+		pubkeys = StagePubkeys
+		extensions = StageRewardExtensions
+	case "prod":
+		pubkeys = ProdPubkeys
+		extensions = ProdRewardExtensions
 	}
 
-	// caplitalize pubkeys in rewards
-	for i, reward := range rewards {
-		for j, claimWallet := range reward.ClaimWallets {
-			rewards[i].ClaimWallets[j] = strings.ToUpper(claimWallet)
+	// Capitalize pubkeys
+	for i := range pubkeys {
+		pubkeys[i] = strings.ToUpper(pubkeys[i])
+	}
+
+	// Assign pubkeys to all base rewards
+	for i := range rewards {
+		rewards[i].ClaimWallets = pubkeys
+	}
+
+	// Add environment-specific rewards
+	if len(extensions) > 0 {
+		// Create a copy of extensions to avoid modifying the original
+		extendedRewards := make([]Reward, len(extensions))
+		copy(extendedRewards, extensions)
+
+		// Assign pubkeys to extended rewards
+		for i := range extendedRewards {
+			extendedRewards[i].ClaimWallets = pubkeys
 		}
+
+		// Append extended rewards to base rewards
+		rewards = append(rewards, extendedRewards...)
 	}
 
 	return &RewardService{
