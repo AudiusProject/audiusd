ce41a132004fe91e36cf5de58258e4cbb24e5d3d
Alec Savvy <alecsavvy@outlook.com>
2025-04-18 10:03:43 -0600

    passing tests

--- pkg/core/test/integration/connect_test.go
+++ pkg/core/test/integration/connect_test.go
@@ -14,7 +14,7 @@ func TestConnectRPC(t *testing.T) {
 	t.Run("should return a health response", func(t *testing.T) {
 		ctx := context.Background()
 
-		systemClient := systemv1connect.NewSystemServiceClient(http.DefaultClient, "https://node1.audiusd.devnet")
+		systemClient := systemv1connect.NewSystemServiceClient(http.DefaultClient, "http://audiusd-1")
 		res, err := systemClient.GetHealth(ctx, connect.NewRequest(&systemv1.GetHealthRequest{}))
 		if err != nil {
 			t.Errorf("unexpected error: %v", err)
