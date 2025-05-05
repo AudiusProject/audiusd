2eb87556527a69505d750e3fbf2747f76a4e2a21
Alec Savvy <alecsavvy@outlook.com>
2025-05-05 13:05:31 -0600

    simplify middleware

--- pkg/common/ip.go
+++ pkg/common/ip.go
@@ -2,14 +2,13 @@ package common
 
 import (
 	"context"
-	"net"
-	"net/http"
-	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
-const ClientIPKey = "client-ip"
+type contextKey string
+
+const ClientIPKey contextKey = "client-ip"
 
 func GetClientIP(ctx context.Context) string {
 	val := ctx.Value(ClientIPKey)
@@ -28,26 +27,10 @@ func GetClientIP(ctx context.Context) string {
 func InjectRealIP() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			ip := ExtractRealIP(c.Request())
+			ip := c.RealIP()
 			ctx := context.WithValue(c.Request().Context(), ClientIPKey, ip)
 			c.SetRequest(c.Request().WithContext(ctx))
 			return next(c)
 		}
 	}
-}
-
-func ExtractRealIP(r *http.Request) string {
-	if xff := r.Header.Get("X-Forwarded-For"); xff != "" {
-		// Only use the first IP in the list
-		parts := strings.Split(xff, ",")
-		return strings.TrimSpace(parts[0])
-	}
-	if xrip := r.Header.Get("X-Real-IP"); xrip != "" {
-		return xrip
-	}
-	host, _, err := net.SplitHostPort(r.RemoteAddr)
-	if err == nil {
-		return host
-	}
-	return r.RemoteAddr
 }
