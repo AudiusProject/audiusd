16a1937d167b4b93f1b3bc3d4bec4420bb61ff62
Alec Savvy <alecsavvy@outlook.com>
2025-05-05 14:42:34 -0600

    revert serve_upload

--- pkg/mediorum/server/serve_upload_grpc.go
+++ pkg/mediorum/server/serve_upload_grpc.go
@@ -1,10 +1,11 @@
 package server
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"io"
-	"mime/multipart"
 	"net/http"
 	"os"
 	"strconv"
@@ -14,6 +15,8 @@ import (
 	"github.com/AudiusProject/audiusd/pkg/mediorum/cidutil"
 	"github.com/AudiusProject/audiusd/pkg/mediorum/server/signature"
 
+	"mime/multipart"
+
 	"github.com/labstack/echo/v4"
 	"github.com/oklog/ulid/v2"
 	"golang.org/x/exp/slices"
@@ -22,33 +25,62 @@ import (
 
 var (
 	filesFormFieldName = "files"
+
+	errNotFoundError      = errors.New("not found")
+	errUnprocessableError = errors.New("unprocessable")
+
+	ErrDiskFull                                 = errors.New("disk is too full to accept new uploads")
+	ErrInvalidTemplate                          = errors.New("invalid template")
+	ErrUploadToPlacementHosts                   = errors.New("if placement_hosts is specified, you must upload to one of the placement_hosts")
+	ErrAllPlacementHostsMustBeRegisteredSigners = errors.New("all placement_hosts must be registered signers")
+	ErrInvalidPreviewStartSeconds               = errors.New("invalid preview start seconds")
+	ErrUploadProcessFailed                      = errors.New("upload process failed")
 )
 
 func (ss *MediorumServer) serveUploadDetail(c echo.Context) error {
-	var upload *Upload
-	err := ss.crud.DB.First(&upload, "id = ?", c.Param("id")).Error
+	id := c.Param("id")
+	fix := c.QueryParam("fix") == "true"
+	analyze := c.QueryParam("analyze") == "true"
+
+	upload, err := ss.serveUpload(id, fix, analyze)
 	if err != nil {
-		return echo.NewHTTPError(404, err.Error())
-	}
-	if upload.Status == JobStatusError {
-		return c.JSON(422, upload)
-	}
-
-	if fix, _ := strconv.ParseBool(c.QueryParam("fix")); fix && upload.Status != JobStatusDone {
-		err = ss.transcode(upload)
-		if err != nil {
-			return err
+		if errors.Is(err, errNotFoundError) {
+			return c.JSON(404, err.Error())
 		}
-	}
-
-	if analyze, _ := strconv.ParseBool(c.QueryParam("analyze")); analyze && upload.AudioAnalysisStatus != "done" {
-		err = ss.analyzeAudio(upload, time.Minute*10)
-		if err != nil {
-			return err
+		if errors.Is(err, errUnprocessableError) {
+			return c.JSON(422, upload)
 		}
+		return err
 	}
 
 	return c.JSON(200, upload)
+}
+
+func (ss *MediorumServer) serveUpload(id string, fix bool, analyze bool) (*Upload, error) {
+	var upload *Upload
+	err := ss.crud.DB.First(&upload, "id = ?", id).Error
+	if err != nil {
+		return nil, errors.Join(errNotFoundError, err)
+	}
+	if upload.Status == JobStatusError {
+		return upload, errors.Join(errUnprocessableError, err)
+	}
+
+	if fix && upload.Status != JobStatusDone {
+		err = ss.transcode(upload)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if analyze && upload.AudioAnalysisStatus != "done" {
+		err = ss.analyzeAudio(upload, time.Minute*10)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return upload, nil
 }
 
 func (ss *MediorumServer) serveUploadList(c echo.Context) error {
@@ -140,10 +172,72 @@ func (ss *MediorumServer) updateUpload(c echo.Context) error {
 	return c.JSON(200, upload)
 }
 
+type multipartFileReader struct {
+	file *multipart.FileHeader
+}
+
+func (f *multipartFileReader) Filename() string {
+	return f.file.Filename
+}
+
+func (f *multipartFileReader) Open() (io.ReadCloser, error) {
+	return f.file.Open()
+}
+
 func (ss *MediorumServer) postUpload(c echo.Context) error {
+	qsig := c.QueryParam("signature")
+	userWalletHeader := c.Request().Header.Get("X-User-Wallet-Addr")
+	ftemplate := c.FormValue("template")
+	previewStart := c.FormValue("previewStartSeconds")
+	fPlacementHosts := c.FormValue("placement_hosts")
+
+	form, err := c.MultipartForm()
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	formFiles := form.File[filesFormFieldName]
+	files := make([]FileReader, len(formFiles))
+	for i, file := range formFiles {
+		files[i] = &multipartFileReader{file: file}
+	}
+	defer form.RemoveAll()
+
+	uploads, err := ss.uploadFile(c.Request().Context(), qsig, userWalletHeader, ftemplate, previewStart, fPlacementHosts, files)
+	if err != nil {
+		if errors.Is(err, ErrDiskFull) {
+			return c.String(http.StatusServiceUnavailable, "disk is too full to accept new uploads")
+		}
+		if errors.Is(err, ErrInvalidTemplate) {
+			return c.String(http.StatusBadRequest, "invalid template")
+		}
+		if errors.Is(err, ErrUploadToPlacementHosts) {
+			return c.String(http.StatusBadRequest, "if placement_hosts is specified, you must upload to one of the placement_hosts")
+		}
+		if errors.Is(err, ErrAllPlacementHostsMustBeRegisteredSigners) {
+			return c.String(http.StatusBadRequest, "all placement_hosts must be registered signers")
+		}
+		if errors.Is(err, ErrInvalidPreviewStartSeconds) {
+			return c.String(http.StatusBadRequest, "invalid preview start seconds")
+		}
+		if errors.Is(err, ErrUploadProcessFailed) {
+			return c.String(422, "upload process failed")
+		}
+		return err
+	}
+
+	return c.JSON(200, uploads)
+}
+
+type FileReader interface {
+	Filename() string
+	Open() (io.ReadCloser, error)
+}
+
+func (ss *MediorumServer) uploadFile(ctx context.Context, qsig string, userWalletHeader string, ftemplate string, previewStart string, fPlacementHosts string, files []FileReader) ([]*Upload, error) {
 	if !ss.diskHasSpace() {
 		ss.logger.Warn("disk is too full to accept new uploads")
-		return c.String(http.StatusServiceUnavailable, "disk is too full to accept new uploads")
+		return nil, ErrDiskFull
 	}
 
 	// read user wallet from ?signature query string
@@ -153,13 +247,12 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 	// updateUpload uses the requireUserSignature c.Get("signer-wallet")
 	// but requireUserSignature will fail request if missing
 	// so parse direclty here
-	if sig, err := signature.ParseFromQueryString(c.QueryParam("signature")); err == nil {
+	if sig, err := signature.ParseFromQueryString(qsig); err == nil {
 		userWallet = sql.NullString{
 			String: sig.SignerWallet,
 			Valid:  true,
 		}
 	} else {
-		userWalletHeader := c.Request().Header.Get("X-User-Wallet-Addr")
 		if userWalletHeader != "" {
 			userWallet = sql.NullString{
 				String: userWalletHeader,
@@ -168,27 +261,21 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 		}
 	}
 
-	// Multipart form
-	form, err := c.MultipartForm()
-	if err != nil {
-		return err
-	}
-	template := JobTemplate(c.FormValue("template"))
+	template := JobTemplate(ftemplate)
 	selectedPreview := sql.NullString{Valid: false}
-	previewStart := c.FormValue("previewStartSeconds")
 
 	if err := validateJobTemplate(template); err != nil {
-		return c.String(400, err.Error())
+		return nil, err
 	}
 
 	var placementHosts []string = nil
-	if v := c.FormValue("placement_hosts"); v != "" {
-		placementHosts = strings.Split(v, ",")
+	if fPlacementHosts != "" {
+		placementHosts = strings.Split(fPlacementHosts, ",")
 	}
 
 	if placementHosts != nil {
 		if !slices.Contains(placementHosts, ss.Config.Self.Host) {
-			return c.String(400, "if placement_hosts is specified, you must upload to one of the placement_hosts")
+			return nil, ErrUploadToPlacementHosts
 		}
 		// validate that the placement hosts are all registered nodes
 		for _, host := range placementHosts {
@@ -200,7 +287,7 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 				}
 			}
 			if !isRegistered {
-				return c.String(400, "all placement_hosts must be registered signers")
+				return nil, ErrAllPlacementHostsMustBeRegisteredSigners
 			}
 		}
 	}
@@ -208,7 +295,7 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 	if previewStart != "" {
 		previewStartSeconds, err := strconv.ParseFloat(previewStart, 64)
 		if err != nil {
-			return c.String(http.StatusBadRequest, "error parsing previewStartSeconds: "+err.Error())
+			return nil, ErrInvalidPreviewStartSeconds
 		}
 		selectedPreviewString := fmt.Sprintf("320_preview|%g", previewStartSeconds)
 		selectedPreview = sql.NullString{
@@ -216,8 +303,6 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 			String: selectedPreviewString,
 		}
 	}
-	files := form.File[filesFormFieldName]
-	defer form.RemoveAll()
 
 	// each file:
 	// - hash contents
@@ -225,13 +310,14 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 	// - some task queue stuff
 
 	uploads := make([]*Upload, len(files))
-	wg, _ := errgroup.WithContext(c.Request().Context())
+	wg, _ := errgroup.WithContext(ctx)
 	for idx, formFile := range files {
 
 		idx := idx
 		formFile := formFile
 		wg.Go(func() error {
 			now := time.Now().UTC()
+			filename := formFile.Filename()
 			upload := &Upload{
 				ID:               ulid.Make().String(),
 				UserWallet:       userWallet,
@@ -241,7 +327,7 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 				CreatedBy:        ss.Config.Self.Host,
 				CreatedAt:        now,
 				UpdatedAt:        now,
-				OrigFileName:     formFile.Filename,
+				OrigFileName:     filename,
 				TranscodeResults: map[string]string{},
 				PlacementHosts:   placementHosts,
 			}
@@ -267,11 +353,11 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 			if err != nil {
 				// fail upload if ffprobe fails
 				upload.Error = err.Error()
-				return c.String(400, err.Error())
+				return err
 			}
 
 			// ffprobe: restore orig filename
-			upload.FFProbe.Format.Filename = formFile.Filename
+			upload.FFProbe.Format.Filename = filename
 
 			// replicate to my bucket + others
 			ss.replicateToMyBucket(formFileCID, tmpFile)
@@ -281,7 +367,7 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 				return err
 			}
 
-			ss.logger.Info("mirrored", "name", formFile.Filename, "uploadID", upload.ID, "cid", formFileCID, "mirrors", upload.Mirrors)
+			ss.logger.Info("mirrored", "name", filename, "uploadID", upload.ID, "cid", formFileCID, "mirrors", upload.Mirrors)
 
 			if template == JobTemplateImgSquare || template == JobTemplateImgBackdrop {
 				upload.TranscodeResults["original.jpg"] = formFileCID
@@ -297,16 +383,15 @@ func (ss *MediorumServer) postUpload(c echo.Context) error {
 		})
 	}
 
-	status := 200
 	if err := wg.Wait(); err != nil {
 		ss.logger.Error("failed to process new upload", "err", err)
-		status = 422
+		return nil, ErrUploadProcessFailed
 	}
 
-	return c.JSON(status, uploads)
+	return uploads, nil
 }
 
-func copyUploadToTempFile(file *multipart.FileHeader) (*os.File, error) {
+func copyUploadToTempFile(file FileReader) (*os.File, error) {
 	temp, err := os.CreateTemp("", "mediorumUpload")
 	if err != nil {
 		return nil, err
