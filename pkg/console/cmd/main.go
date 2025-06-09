35722e25a4f39a1344ebe9ba081d95a9b700f918
Alec Savvy <alecjsavvy@gmail.com>
2025-06-09 12:43:59 -0600

    setup

---
+++ pkg/console/cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/AudiusProject/audiusd/pkg/common"
+	"github.com/AudiusProject/audiusd/pkg/console"
+	"github.com/AudiusProject/audiusd/pkg/etl"
+	"github.com/AudiusProject/audiusd/pkg/sdk"
+)
+
+func main() {
+	logger := common.NewLogger(nil)
+
+	logger.Info("Starting Console")
+
+	auds := sdk.NewAudiusdSDK("http://localhost:3000")
+
+	etl := etl.NewETLService(auds.Core, logger)
+	etl.SetDBURL("postgres://postgres:postgres@0.0.0.0:5432/audiusd?sslmode=disable")
+
+	console := console.NewConsole(etl)
+	console.SetupRoutes()
+
+	defer console.Stop()
+	if err := console.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
--- pkg/console/console.go
+++ pkg/console/console.go
@@ -3,11 +3,21 @@ package console
 import (
 	"context"
 	"net/http"
+	"strings"
 
+	"github.com/AudiusProject/audiusd/pkg/console/templates/pages"
 	"github.com/AudiusProject/audiusd/pkg/etl"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/sync/errgroup"
+
+	"embed"
 )
+
+//go:embed assets/css
+var cssFS embed.FS
+
+//go:embed assets/images
+var imagesFS embed.FS
 
 type Console struct {
 	e   *echo.Echo
@@ -15,21 +25,43 @@ type Console struct {
 }
 
 func NewConsole(etl *etl.ETLService) *Console {
-	return &Console{etl: etl}
+	return &Console{etl: etl, e: echo.New()}
 }
 
 func (con *Console) SetupRoutes() {
 	e := con.e
+	e.HideBanner = true
 
-	e.GET("/", con.stubRoute)
+	// Add cache control middleware for static assets
+	cacheControl := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			path := c.Request().URL.Path
+			// Only apply caching to image files
+			if strings.HasPrefix(path, "/assets/") && (strings.HasSuffix(path, ".svg") || strings.HasSuffix(path, ".png") || strings.HasSuffix(path, ".jpg") || strings.HasSuffix(path, ".jpeg") || strings.HasSuffix(path, ".gif")) {
+				c.Response().Header().Set("Cache-Control", "public, max-age=604800") // Cache for 1 week
+			}
+			return next(c)
+		}
+	}
 
-	e.GET("/validators", con.stubRoute)
+	cssHandler := echo.MustSubFS(cssFS, "assets/css")
+	imagesHandler := echo.MustSubFS(imagesFS, "assets/images")
+	e.StaticFS("/assets/css", cssHandler)
+	e.StaticFS("/assets/images", imagesHandler)
+
+	// Apply cache control middleware to static asset routes
+	e.Use(cacheControl)
+
+	e.GET("/", con.Dashboard)
+	e.GET("/hello", con.Hello)
+
+	e.GET("/validators", con.Validators)
 	e.GET("/validator/:address", con.stubRoute)
 
-	e.GET("/blocks", con.stubRoute)
+	e.GET("/blocks", con.Blocks)
 	e.GET("/block/:height", con.stubRoute)
 
-	e.GET("/transactions", con.stubRoute)
+	e.GET("/transactions", con.Transactions)
 	e.GET("/transaction/:hash", con.stubRoute)
 
 	e.GET("/account/:address", con.stubRoute)
@@ -37,23 +69,70 @@ func (con *Console) SetupRoutes() {
 	e.GET("/account/:address/uploads", con.stubRoute)
 	e.GET("/account/:address/releases", con.stubRoute)
 
-	e.GET("/upload/:address", con.stubRoute)
+	e.GET("/content", con.Content)
+	e.GET("/content/:address", con.Content)
 
 	e.GET("/release/:address", con.stubRoute)
+
+	e.GET("/search", con.stubRoute)
 }
 
 func (con *Console) Run() error {
 	g, _ := errgroup.WithContext(context.Background())
 
 	g.Go(func() error {
-		return con.etl.Run()
+		if err := con.etl.Run(); err != nil {
+			return err
+		}
+		return nil
 	})
 
 	g.Go(func() error {
-		return con.e.Start(":8080")
+		if err := con.e.Start(":8080"); err != nil {
+			return err
+		}
+		return nil
 	})
 
 	return g.Wait()
+}
+
+func (con *Console) Stop() {
+	con.e.Shutdown(context.Background())
+}
+
+func (con *Console) Hello(c echo.Context) error {
+	param := "sup"
+	if name := c.QueryParam("name"); name != "" {
+		param = name
+	}
+	p := pages.Hello(param)
+	return p.Render(c.Request().Context(), c.Response().Writer)
+}
+
+func (con *Console) Dashboard(c echo.Context) error {
+	p := pages.Dashboard()
+	return p.Render(c.Request().Context(), c.Response().Writer)
+}
+
+func (con *Console) Validators(c echo.Context) error {
+	p := pages.Validators()
+	return p.Render(c.Request().Context(), c.Response().Writer)
+}
+
+func (con *Console) Blocks(c echo.Context) error {
+	p := pages.Blocks()
+	return p.Render(c.Request().Context(), c.Response().Writer)
+}
+
+func (con *Console) Transactions(c echo.Context) error {
+	p := pages.Transactions()
+	return p.Render(c.Request().Context(), c.Response().Writer)
+}
+
+func (con *Console) Content(c echo.Context) error {
+	p := pages.Content()
+	return p.Render(c.Request().Context(), c.Response().Writer)
 }
 
 func (con *Console) stubRoute(c echo.Context) error {
