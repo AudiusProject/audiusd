b66081e2fe437a55ca9a6c0b9d324465eaba6f93
Alec Savvy <alecsavvy@outlook.com>
2025-06-11 12:55:23 -0600

    yeah

--- pkg/console/templates/pages/hello_templ.go
+++ pkg/console/templates/pages/hello_templ.go
@@ -43,14 +43,14 @@ func Hello(name string) templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"text-3xl font-bold underline\">Sup, ")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"text-3xl font-bold underline text-gray-900 dark:text-gray-100\">Sup, ")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var3 string
 			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(name)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/pages/hello.templ`, Line: 7, Col: 55}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/pages/hello.templ`, Line: 7, Col: 88}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
