6f66e50be0163b392d8eab3080d4f07393ef1bdc
Alec Savvy <alecsavvy@outlook.com>
2025-09-25 14:59:48 -0600

    test comments

--- pkg/core/server/eip712_test.go
+++ pkg/core/server/eip712_test.go
@@ -18,7 +18,6 @@ func TestEIP712(t *testing.T) {
 			Action:     "Update",
 			Metadata:   `{"cid":"bagaaiera2c6skimqkebrc2rmb6i5sx4a7zbglckodqzve7ot6kjxxrschnlq","data":{"name":"Matcha Male","handle":"matchamale","bio":"matcha is lyfe","location":"Aurora, CO","events":{},"is_deactivated":false,"allow_ai_attribution":false,"spl_usdc_payout_wallet":null}}`,
 			Signature:  "0xd654719ec699cd00e06afb73bf7e1155c64b736bab9905f098ef11927c04afe82a90e72e8202592d6ad5ba6c2e1eac3120e34f4caea2a36d63f28434efa655551c",
-			Signer:     "",
 			Nonce:      "0x8172f237c0ff351c2478fbb8ac5a21c4aa179304f8550e782284828191adf142",
 		}
 
@@ -41,7 +40,6 @@ func TestEIP712(t *testing.T) {
 			Action:     "Update",
 			Metadata:   `{"cid":"bagaaiera5eo2dqk6iuumfwi7gwvhgg57pn5qyg75njezxgieacnkf7qsohqq","data":{"name":"yayayayayay","handle":"yayayayayay","bio":"howdy pardner","location":"Denver, CO","events":{},"is_deactivated":false,"allow_ai_attribution":false}}`,
 			Signature:  "0x217729f7823deb28aeeaa9451e25e0e21cffb073bc455b900382d9d5c25106b858c099248c12d40beaada4d7c0580dd7e3886ea9d06d2f6dc06ed66ceb2455e11b",
-			Signer:     "",
 			Nonce:      "0x29a8bf36312f3aa4cf2257ceae6d2bb893f27d41372b763223ca9e7de93777d8",
 		}
 
